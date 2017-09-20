c7aeda27a02ecde24615816f63ce5bcab6e48311
Nicholas Clark <nicholas.clark@gmail.com>
2017-09-20 16:03:23 -0600

    Formatted test-code

--- test/crypto_box.c
+++ test/crypto_box.c
@@ -1,181 +1,190 @@
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
 #include <unistd.h>
 
 #include "tweetnacl.h"
-
 
 typedef unsigned char UCHAR;
 
 void randombytes(UCHAR buffer[], unsigned long long size)
 {
-	int fd;
+    int fd;
 
-	fd = open( "/dev/urandom", O_RDONLY );
-	if( fd < 0 ) {
-		fprintf( stderr, "Failed to open /dev/urandom\n" );
-		exit(1);
-	}
+    fd = open("/dev/urandom", O_RDONLY);
+    if (fd < 0) {
+        fprintf(stderr, "Failed to open /dev/urandom\n");
+        exit(1);
+    }
 
-	int rc;
-	if( (rc = read( fd, buffer, size )) >= 0 ) {
-		close( fd );
-	}
+    int rc;
+    if ((rc = read(fd, buffer, size)) >= 0) {
+        close(fd);
+    }
 }
 
-char* to_hex( char hex[], const UCHAR bin[], size_t length )
+char *to_hex(char hex[], const UCHAR bin[], size_t length)
 {
-	size_t i;
-	UCHAR *p0 = (UCHAR *)bin;
-	char *p1 = hex;
+    size_t i;
+    UCHAR *p0 = (UCHAR *)bin;
+    char *p1 = hex;
 
-	for( i = 0; i < length; i++ ) {
-		snprintf( p1, 3, "%02x", *p0 );
-		p0 += 1;
-		p1 += 2;
-	}
+    for (i = 0; i < length; i++) {
+        snprintf(p1, 3, "%02x", *p0);
+        p0 += 1;
+        p1 += 2;
+    }
 
-	return hex;
+    return hex;
 }
 
-int is_zero( const UCHAR *data, int len )
+int is_zero(const UCHAR *data, int len)
 {
-	int i;
-	int rc;
+    int i;
+    int rc;
 
-	rc = 0;
-	for(i = 0; i < len; ++i) {
-		rc |= data[i];
-	}
+    rc = 0;
+    for (i = 0; i < len; ++i) {
+        rc |= data[i];
+    }
 
-	return rc;
+    return rc;
 }
 
 #define MAX_MSG_SIZE 1400
 
-int encrypt(UCHAR encrypted[], const UCHAR pk[], const UCHAR sk[], const UCHAR nonce[], const UCHAR plain[], int length)
+int encrypt(UCHAR encrypted[], const UCHAR pk[], const UCHAR sk[],
+            const UCHAR nonce[], const UCHAR plain[], int length)
 {
-	UCHAR temp_plain[MAX_MSG_SIZE];
-	UCHAR temp_encrypted[MAX_MSG_SIZE];
-	int rc;
+    UCHAR temp_plain[MAX_MSG_SIZE];
+    UCHAR temp_encrypted[MAX_MSG_SIZE];
+    int rc;
 
-	printf("encrypt\n");
+    printf("encrypt\n");
 
-	if(length+crypto_box_ZEROBYTES >= MAX_MSG_SIZE) {
-		return -2;
-	}
+    if (length + crypto_box_ZEROBYTES >= MAX_MSG_SIZE) {
+        return -2;
+    }
 
-	memset(temp_plain, '\0', crypto_box_ZEROBYTES);
-	memcpy(temp_plain + crypto_box_ZEROBYTES, plain, length);
+    memset(temp_plain, '\0', crypto_box_ZEROBYTES);
+    memcpy(temp_plain + crypto_box_ZEROBYTES, plain, length);
 
-	rc = crypto_box(temp_encrypted, temp_plain, crypto_box_ZEROBYTES + length, nonce, pk, sk);
+    rc = crypto_box(temp_encrypted, temp_plain, crypto_box_ZEROBYTES + length,
+                    nonce, pk, sk);
 
-	if( rc != 0 ) {
-		return -1;
-	}
+    if (rc != 0) {
+        return -1;
+    }
 
-	if( is_zero(temp_plain, crypto_box_BOXZEROBYTES) != 0 ) {
-		return -3;
-	}
+    if (is_zero(temp_plain, crypto_box_BOXZEROBYTES) != 0) {
+        return -3;
+    }
 
-	memcpy(encrypted, temp_encrypted + crypto_box_BOXZEROBYTES, crypto_box_ZEROBYTES + length);
+    memcpy(encrypted, temp_encrypted + crypto_box_BOXZEROBYTES,
+           crypto_box_ZEROBYTES + length);
 
-	return crypto_box_ZEROBYTES + length - crypto_box_BOXZEROBYTES;
+    return crypto_box_ZEROBYTES + length - crypto_box_BOXZEROBYTES;
 }
 
-int decrypt(UCHAR plain[], const UCHAR pk[], const UCHAR sk[], const UCHAR nonce[], const UCHAR encrypted[], int length)
+int decrypt(UCHAR plain[], const UCHAR pk[], const UCHAR sk[],
+            const UCHAR nonce[], const UCHAR encrypted[], int length)
 {
-	UCHAR temp_encrypted[MAX_MSG_SIZE];
-	UCHAR temp_plain[MAX_MSG_SIZE];
-	int rc;
+    UCHAR temp_encrypted[MAX_MSG_SIZE];
+    UCHAR temp_plain[MAX_MSG_SIZE];
+    int rc;
 
-	printf("decrypt\n");
+    printf("decrypt\n");
 
-	if(length+crypto_box_BOXZEROBYTES >= MAX_MSG_SIZE) {
-		return -2;
-	}
+    if (length + crypto_box_BOXZEROBYTES >= MAX_MSG_SIZE) {
+        return -2;
+    }
 
-	memset(temp_encrypted, '\0', crypto_box_BOXZEROBYTES);
-	memcpy(temp_encrypted + crypto_box_BOXZEROBYTES, encrypted, length);
+    memset(temp_encrypted, '\0', crypto_box_BOXZEROBYTES);
+    memcpy(temp_encrypted + crypto_box_BOXZEROBYTES, encrypted, length);
 
-	rc = crypto_box_open(temp_plain, temp_encrypted, crypto_box_BOXZEROBYTES + length, nonce, pk, sk);
+    rc = crypto_box_open(temp_plain, temp_encrypted,
+                         crypto_box_BOXZEROBYTES + length, nonce, pk, sk);
 
-	if( rc != 0 ) {
-		return -1;
-	}
+    if (rc != 0) {
+        return -1;
+    }
 
-	if( is_zero(temp_plain, crypto_box_ZEROBYTES) != 0 ) {
-		return -3;
-	}
+    if (is_zero(temp_plain, crypto_box_ZEROBYTES) != 0) {
+        return -3;
+    }
 
-	memcpy(plain, temp_plain + crypto_box_ZEROBYTES, crypto_box_BOXZEROBYTES + length);
+    memcpy(plain, temp_plain + crypto_box_ZEROBYTES,
+           crypto_box_BOXZEROBYTES + length);
 
-	return crypto_box_BOXZEROBYTES + length - crypto_box_ZEROBYTES;
+    return crypto_box_BOXZEROBYTES + length - crypto_box_ZEROBYTES;
 }
 
 typedef struct {
-	char* name;
-	UCHAR public_key[crypto_box_PUBLICKEYBYTES];
-	UCHAR secret_key[crypto_box_SECRETKEYBYTES];
+    char *name;
+    UCHAR public_key[crypto_box_PUBLICKEYBYTES];
+    UCHAR secret_key[crypto_box_SECRETKEYBYTES];
 } User;
 
-User *new_user(char* name)
+User *new_user(char *name)
 {
-	User* user;
+    User *user;
 
-	user = (User*) malloc(sizeof(User));
-	user->name = name;
+    user = (User *)malloc(sizeof(User));
+    user->name = name;
 
-	crypto_box_keypair(user->public_key, user->secret_key);
+    crypto_box_keypair(user->public_key, user->secret_key);
 
-	return user;
+    return user;
 }
 
 void print_user(User *user)
 {
-	char phexbuf[2*crypto_box_PUBLICKEYBYTES+1];
-	char shexbuf[2*crypto_box_SECRETKEYBYTES+1];
+    char phexbuf[2 * crypto_box_PUBLICKEYBYTES + 1];
+    char shexbuf[2 * crypto_box_SECRETKEYBYTES + 1];
 
-	printf("username: %s\n", user->name);
-	printf("public key: %s\n", to_hex(phexbuf, user->public_key, crypto_box_PUBLICKEYBYTES ));
-	printf("secret key: %s\n\n", to_hex(shexbuf, user->secret_key, crypto_box_SECRETKEYBYTES ));
+    printf("username: %s\n", user->name);
+    printf("public key: %s\n",
+           to_hex(phexbuf, user->public_key, crypto_box_PUBLICKEYBYTES));
+    printf("secret key: %s\n\n",
+           to_hex(shexbuf, user->secret_key, crypto_box_SECRETKEYBYTES));
 }
 
 int main()
 {
-	char hexbuf[256];
+    char hexbuf[256];
 
-	int rc;
-	User *bob = new_user("bob");
-	User *eve = new_user("eve");
-	char msg[] = "Hello";
+    int rc;
+    User *bob = new_user("bob");
+    User *eve = new_user("eve");
+    char msg[] = "Hello";
 
-	UCHAR nonce[crypto_box_NONCEBYTES];
-	randombytes(nonce, crypto_box_NONCEBYTES);
+    UCHAR nonce[crypto_box_NONCEBYTES];
+    randombytes(nonce, crypto_box_NONCEBYTES);
 
-	print_user(bob);
-	print_user(eve);
+    print_user(bob);
+    print_user(eve);
 
-	printf("message: %s\n", msg);
+    printf("message: %s\n", msg);
 
-	UCHAR encrypted[1000];
-	rc = encrypt(encrypted, bob->public_key, eve->secret_key, nonce, (UCHAR *)msg, strlen(msg));
-	if( rc < 0 ) {
-		return 1;
-	}
-	printf("encrypted: %s\n", to_hex(hexbuf, encrypted, rc ));
+    UCHAR encrypted[1000];
+    rc = encrypt(encrypted, bob->public_key, eve->secret_key, nonce,
+                 (UCHAR *)msg, strlen(msg));
+    if (rc < 0) {
+        return 1;
+    }
+    printf("encrypted: %s\n", to_hex(hexbuf, encrypted, rc));
 
-	UCHAR decrypted[1000];
-	rc = decrypt(decrypted, eve->public_key, bob->secret_key, nonce, encrypted, rc);
-	if( rc < 0 ) {
-		return 1;
-	}
+    UCHAR decrypted[1000];
+    rc = decrypt(decrypted, eve->public_key, bob->secret_key, nonce, encrypted,
+                 rc);
+    if (rc < 0) {
+        return 1;
+    }
 
-	decrypted[rc] = '\0';
-	printf("decrypted: %s\n", decrypted);
+    decrypted[rc] = '\0';
+    printf("decrypted: %s\n", decrypted);
 
     if (memcmp(msg, decrypted, sizeof(msg) - 1) != 0) {
         printf("Error: message mismatch!\n");
@@ -183,5 +192,5 @@ int main()
     }
 
     printf("Tests passed OK\n");
-	return 0;
+    return 0;
 }
--- test/crypto_sign.c
+++ test/crypto_sign.c
@@ -1,60 +1,62 @@
 
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "tweetnacl.h"
 
 typedef unsigned char UCHAR;
- 
+
 #define MAX_MSG_LEN 64
 
+int sign(UCHAR sm[], const UCHAR m[], const int mlen, const UCHAR sk[])
+{
+    unsigned long long smlen;
 
-int sign(UCHAR sm[], const UCHAR m[], const int mlen, const UCHAR sk[]) {
-	unsigned long long smlen;
-	
-	if( crypto_sign(sm,&smlen, m, mlen, sk) == 0) {
-		return smlen;
-	} else {
-		return -1;
-	}
+    if (crypto_sign(sm, &smlen, m, mlen, sk) == 0) {
+        return smlen;
+    } else {
+        return -1;
+    }
 }
 
-int verify(UCHAR m[], const UCHAR sm[], const int smlen, const UCHAR pk[]) {
-	unsigned long long mlen;
+int verify(UCHAR m[], const UCHAR sm[], const int smlen, const UCHAR pk[])
+{
+    unsigned long long mlen;
 
-	if( crypto_sign_open(m, &mlen, sm, smlen, pk) == 0) {
-		return mlen;
-	} else {
-		return -1;
-	}
+    if (crypto_sign_open(m, &mlen, sm, smlen, pk) == 0) {
+        return mlen;
+    } else {
+        return -1;
+    }
 }
 
-int main() {
-	UCHAR sk[crypto_sign_SECRETKEYBYTES];
-	UCHAR pk[crypto_sign_PUBLICKEYBYTES];
-	UCHAR sm[MAX_MSG_LEN+crypto_sign_BYTES];
-	UCHAR m[MAX_MSG_LEN+crypto_sign_BYTES];
+int main()
+{
+    UCHAR sk[crypto_sign_SECRETKEYBYTES];
+    UCHAR pk[crypto_sign_PUBLICKEYBYTES];
+    UCHAR sm[MAX_MSG_LEN + crypto_sign_BYTES];
+    UCHAR m[MAX_MSG_LEN + crypto_sign_BYTES];
 
-	memset(m, '\0', MAX_MSG_LEN);
-	int mlen = snprintf((char *)m, MAX_MSG_LEN, "%s", "Hello World!");
+    memset(m, '\0', MAX_MSG_LEN);
+    int mlen = snprintf((char *)m, MAX_MSG_LEN, "%s", "Hello World!");
 
-	int rc = crypto_sign_keypair(pk, sk);
-	if(rc < 0) {
-		return 1;
-	}
+    int rc = crypto_sign_keypair(pk, sk);
+    if (rc < 0) {
+        return 1;
+    }
 
-	int smlen = sign(sm, m, mlen, sk);
-	if(smlen < 0) {
-		return 1;
-	}
+    int smlen = sign(sm, m, mlen, sk);
+    if (smlen < 0) {
+        return 1;
+    }
 
-	mlen = verify(m, sm, smlen, pk);
-	if(mlen < 0) {
-		return 1;
-	}
+    mlen = verify(m, sm, smlen, pk);
+    if (mlen < 0) {
+        return 1;
+    }
 
-	printf("Verified!\n");
+    printf("Verified!\n");
     printf("Tests passed OK\n");
-	return 0;
+    return 0;
 }
