252f76a380e4e89da2df2e014a06010b79516965
Nicholas Clark <nicholas.clark@gmail.com>
2020-12-14 21:12:18 +0000

    Add canned reference data to tests.
    Add a pregenerated set of data and keys to the test environment.
    This helps provide a sanity-check to ensure that compatibility
    doesn't get broken by accident.

--- src/test/test_crypto.py
+++ src/test/test_crypto.py
@@ -2,8 +2,14 @@
 """ Test suite that uses the 'crypto' wrappers for comparing tweetnacl's
 behavior against libsodium. """
 
+import sys
+import os
+import json
+import copy
+import base64
 import random
 import crypto
+
 
 def random_message(length):
     """ Generates a block of random bytes of a user-specified length. """
@@ -31,13 +37,13 @@ def generate_keys(source):
     return keys
 
 
-def generate_data(source, keys):
+def generate_data(source, keys, msg_length=2**16 + 5):
     """ Generates a set of test data using a pre-existing batch of keys. Uses
     the crypto-variant provided in 'source'. """
     # pylint: disable=too-many-locals
 
     data = {}
-    msg = random_message(2**16 + 5)
+    msg = random_message(msg_length)
     secret, public = \
         keys['box']['sender']['secret'], \
         keys['box']['receiver']['public']
@@ -58,31 +64,31 @@ def generate_data(source, keys):
     mult = source.scalarmult.crypto_scalarmult(scalar, element)
     data['scalarmult'] = {'scalar': scalar, 'element': element, 'mult': mult}
 
-    msg = random_message(2**16 + 5)
+    msg = random_message(msg_length)
     signed = source.sign.crypto_sign(msg, keys['sign']['secret'])
     data['sign'] = {'msg': msg, 'signed': signed}
 
-    msg = random_message(2**16 + 5)
+    msg = random_message(msg_length)
     cypher, nonce = source.secretbox.crypto_secretbox(msg, keys['secretbox'])
     data['secretbox'] = {'msg': msg, 'cypher': cypher, 'nonce': nonce}
 
-    msg = random_message(2**16 + 5)
+    msg = random_message(msg_length)
     cypher, nonce = source.stream.crypto_stream_xor(msg, keys['stream'])
     alt = source.stream.alt_crypto_stream_xor(msg, keys['stream'], nonce)[0]
-    length = (2**16 + 5)
+    length = (msg_length)
     stream = source.stream.crypto_stream(length, keys['stream'], nonce)[0]
     data['stream'] = {'msg': msg, 'alt': alt, 'cypher': cypher, 'nonce': nonce,
                       'length': length, 'stream': stream}
 
-    msg = random_message(2**16 + 5)
+    msg = random_message(msg_length)
     auth = source.auth.crypto_auth(msg, keys['auth'])
     data['auth'] = {'msg': msg, 'auth': auth}
 
-    msg = random_message(2**16 + 5)
+    msg = random_message(msg_length)
     auth = source.onetimeauth.crypto_onetimeauth(msg, keys['onetimeauth'])
     data['onetimeauth'] = {'msg': msg, 'auth': auth}
 
-    msg = random_message(2**16 + 5)
+    msg = random_message(msg_length)
     data['hash'] = {'msg': msg, 'hash': source.misc.crypto_hash(msg)}
 
     return data
@@ -169,10 +175,71 @@ def verify_data(source, data, keys):
     source.onetimeauth.crypto_onetimeauth_verify(msg, auth, key)
 
 
+def parse_dict(node, backward=False):
+    """ Parses a dict of crypto keys and/or crypto data, and converts all
+    values to (or from) base64-encoding. This provides an easy way to generate
+    (or load) test-vectors stored as JSON. """
+
+    if isinstance(node, dict):
+        node = copy.deepcopy(node)
+
+        for key in node:
+            node[key] = parse_dict(node[key], backward)
+
+        return node
+
+    if isinstance(node, (bytes, bytearray)):
+        if backward:
+            return base64.decodebytes(node)
+
+        return base64.encodebytes(node).decode().strip()
+
+    if isinstance(node, str):
+        if backward:
+            return base64.decodebytes(node.encode('latin_1'))
+
+        return base64.encodebytes( node.encode('latin_1')).strip()
+
+    if isinstance(node, (float, int, bool)):
+        return node
+
+    raise ValueError("Unknown datatype of node. %s" % str(node))
+
+
+def generate_reference(outfile=sys.stdout):
+    """ Generates a set of keys and some reference data. Encodes all values to
+    base64 and writes the result as JSON to outfile. """
+
+    keys = generate_keys(crypto.TweetNacl)
+    data = generate_data(crypto.TweetNacl, keys, 2**9)
+    export_data = parse_dict({"keys": keys, "data": data})
+    export_data = json.dumps(export_data, indent=2)
+
+    if outfile.seekable():
+        outfile.seek(0)
+
+    outfile.write(export_data)
+
+
 def main():
     """ Cycle through all permutations of key-generation, data-generation,
     and data verification by tweetnacl and libsodium. If this test passes,
     then tweetnacl and libsodium are assumed to be fully cross-compatible. """
+
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    reference_file = os.path.join(script_dir, "reference.json")
+
+    with open(reference_file) as infile:
+        reference = json.loads(infile.read())
+        reference = parse_dict(reference, backward=True)
+
+    for verify_source in (crypto.TweetNacl, crypto.Sodium):
+        # pylint: disable=protected-access
+        print("====================================")
+        print("Key source:   ", "reference.json")
+        print("Data source:  ", "reference.json")
+        print("Verification: ", verify_source.auth.dll._name)
+        verify_data(verify_source, reference['data'], reference['keys'])
 
     for key_source in (crypto.TweetNacl, crypto.Sodium):
         for data_source in (crypto.TweetNacl, crypto.Sodium):
@@ -188,6 +255,7 @@ def main():
                 print("Tests passed OK.")
 
     print("All tests passed OK.")
+
 
 if __name__ == "__main__":
     main()
